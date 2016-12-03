9f5ca267fc9e67a0fed34efaa31713812672a7f8
Fayland Lam <fayland@gmail.com>
2016-12-03 11:54:24 +0800

    from milla

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+WWW::ModulrFinance - Blah blah blah
+
+# SYNOPSIS
+
+    use WWW::ModulrFinance;
+
+# DESCRIPTION
+
+WWW::ModulrFinance is
+
+# AUTHOR
+
+Fayland Lam <fayland@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2016- Fayland Lam
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/WWW/ModulrFinance.pm
@@ -0,0 +1,39 @@
+package WWW::ModulrFinance;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WWW::ModulrFinance - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use WWW::ModulrFinance;
+
+=head1 DESCRIPTION
+
+WWW::ModulrFinance is
+
+=head1 AUTHOR
+
+Fayland Lam E<lt>fayland@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2016- Fayland Lam
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
